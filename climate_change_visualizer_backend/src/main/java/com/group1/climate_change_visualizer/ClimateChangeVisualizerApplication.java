88231cbd77b4abd7eedb2aae876398feb5d00051
Sampo Ikonen <sampo.ikonen1@gmail.com>
2023-03-14 10:40:43 +0200

    backend init

---
+++ climate_change_visualizer_backend/src/main/java/com/group1/climate_change_visualizer/ClimateChangeVisualizerApplication.java
@@ -0,0 +1,13 @@
+package com.group1.climate_change_visualizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimateChangeVisualizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimateChangeVisualizerApplication.class, args);
+	}
+
+}
---
+++ climate_change_visualizer_backend/src/test/java/com/group1/climate_change_visualizer/ClimateChangeVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.climate_change_visualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimateChangeVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
